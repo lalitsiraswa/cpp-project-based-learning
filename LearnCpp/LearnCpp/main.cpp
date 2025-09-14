a6206a88e7fb105e9ceaa378e7c1f003807b472d
Lavik <lavik@Laviks-MacBook-Pro.local>
2025-09-14 12:34:56 +0530

    Initial Project Commit

---
+++ LearnCpp/LearnCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by Lavik on 14/09/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
