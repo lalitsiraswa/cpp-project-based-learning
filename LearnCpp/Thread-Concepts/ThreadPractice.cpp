0f239ac90d09a1e12f7e65ef2f3c5f41c443fe16
Lavik <lavik@Laviks-MacBook-Pro.local>
2025-09-27 22:17:27 +0530

    scoped_lock

--- LearnCpp/Thread-Concepts/ThreadPractice.cpp
+++ LearnCpp/Thread-Concepts/ThreadPractice.cpp
@@ -2,22 +2,24 @@
 #include <thread>
 #include <mutex>
 
-int counter = 0;
-std::mutex mtx;
+std::mutex mtx1, mtx2;
+int value1 = 0, value2 = 0;
 
-void increment() {
+void safeIncrement() {
     for(int i = 0; i < 100000; i++){
-        std::lock_guard<std::mutex> lock(mtx);
-        counter++;
+        std::scoped_lock<std::mutex, std::mutex> lock(mtx1, mtx2); // Optional: <std::mutex, std::mutex>
+        value1++;
+        value2++;
     }
 }
 
 int main(){
-    std::thread t1(increment);
-    std::thread t2(increment);
+    std::thread t1(safeIncrement);
+    std::thread t2(safeIncrement);
     
     t1.join();
     t2.join();
-    std::cout << "Counter = " << counter << std::endl;
+    
+    std::cout << "Value1 = " << value1 << ", value2 = " << value2 << std::endl;
     return 0;
 }
