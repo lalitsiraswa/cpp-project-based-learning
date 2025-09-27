88a882a4578bc20fa9d3a30c0e189489c2fc6f55
Lavik <lavik@Laviks-MacBook-Pro.local>
2025-09-27 16:07:23 +0530

    Using Lambda with thread

--- LearnCpp/Thread-Concepts/ThreadPractice.cpp
+++ LearnCpp/Thread-Concepts/ThreadPractice.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <thread>
 
-void taskFunction(int id){
-    std::cout << "Task " << id << " is running on thread " << std::this_thread::get_id() << std::endl;
-}
-
 int main(){
-    std::thread t1(taskFunction, 1);
-    std::thread t2(taskFunction, 2);
-    t1.join();
-    t2.join();
-    std::cout << "Main thread finished!" << std::endl;
+    std::thread worker([] {
+        std::cout << "Hello from lambda thread!" << std::endl;
+    });
+    worker.join();
     return 0;
 }
